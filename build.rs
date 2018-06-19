eb8dc1d3accb9cd0dc963cb61dadd425af50cba2
arturo castro <arturo@openframeworks.cc>
2018-06-19 14:23:35 +0200

    build in OUT_DIR instead of MANIFEST_DIR

--- build.rs
+++ build.rs
@@ -9,11 +9,12 @@ use std::path::Path;
 fn build_unix() {
 	let zlib_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 	let zlib_native_dir = Path::new(&zlib_dir).join("zlib-1.2.8");
-	let build_dir = zlib_native_dir.join("build");
+	let out_dir = env::var("OUT_DIR").unwrap();
+	let build_dir = Path::new(&out_dir).join("build");
 	fs::remove_dir_all(&build_dir).is_ok();
 	fs::create_dir(&build_dir).is_ok();
 	Command::new("cmake")
-		.arg("..")
+		.arg(zlib_native_dir)
 		.current_dir(&build_dir)
 		.status().unwrap();
 	Command::new("make")
@@ -30,19 +31,19 @@ fn build_unix() {
 fn build_emscripten() {
 	let zlib_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 	let zlib_native_dir = Path::new(&zlib_dir).join("zlib-1.2.8");
-	let build_dir = zlib_native_dir.join("build");
+	let out_dir = env::var("OUT_DIR").unwrap();
+	let build_dir = Path::new(&out_dir).join("build");
 	fs::remove_dir_all(&build_dir).is_ok();
 	fs::create_dir(&build_dir).is_ok();
 	Command::new("emcmake")
 		.arg("cmake")
-		.arg("..")
+		.arg(zlib_native_dir)
 		.current_dir(&build_dir)
 		.status().unwrap();
 	Command::new("emmake")
 		.arg("make")
 		.current_dir(&build_dir)
 		.status().unwrap();
-	let out_dir = env::var("OUT_DIR").unwrap();
 	let dest_path = Path::new(&out_dir).join("libz.a");
 	fs::copy(build_dir.join("libz.a"),dest_path).unwrap();
 	println!("cargo:rustc-link-lib=static=z");
